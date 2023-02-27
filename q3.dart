b749e0870e19a58c5842daf36b086bc071365c6b
GAURAV SINGH JEENA <96417319+gaurav-jeena@users.noreply.github.com>
2023-02-27 11:15:46 +0530

    Add files via upload

--- q3.dart
+++ q3.dart
@@ -1,14 +1,18 @@
-//rotate a number with rotations
-import 'dart:math';
+//print number in digit form
+import 'dart:io';
 
 void main() {
-  int num = 123456;
-  int rotation = 2, temp = num, count = 0;
+  int num = int.parse(stdin.readLineSync()!);
+  int temp = num, digitCount = 1;
   while (num > 0) {
     num = num ~/ 10;
-    count++;
+    digitCount = digitCount * 10;
   }
-  var right = (temp ~/ pow(10, rotation));
-  var left = (temp % pow(10, rotation)) * pow(10, count - rotation);
-  print(left + right);
+  digitCount = digitCount ~/ 10;
+  while (digitCount > 0) {
+    int digit = temp ~/ digitCount;
+    print(digit);
+    temp = temp % digitCount;
+    digitCount = digitCount ~/ 10;
+  }
 }
