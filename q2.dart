44eec7a203143b9ee36dad59f1b838824584fdba
GAURAV SINGH JEENA <96417319+gaurav-jeena@users.noreply.github.com>
2023-02-27 11:08:38 +0530

    Add files via upload

---
+++ q2.dart
@@ -0,0 +1,14 @@
+//rotate a number with rotations
+import 'dart:math';
+
+void main() {
+  int num = 123456;
+  int rotation = 2, temp = num, count = 0;
+  while (num > 0) {
+    num = num ~/ 10;
+    count++;
+  }
+  var right = (temp ~/ pow(10, rotation));
+  var left = (temp % pow(10, rotation)) * pow(10, count - rotation);
+  print(left + right);
+}
